98f7a5714094fb39b7d7af52085d09b0d1bd0006
phoebe velma awuor <awuorphoebi@gmail.com>
2025-01-22 14:45:11 +0300

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,29 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return () => clearTimeout(timer); // Cleanup on unmount or re-render
+  }, [timeRemaining, onAnswered]);
+
+  const { id, prompt, answers, correctIndex } = question;
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
-  const { id, prompt, answers, correctIndex } = question;
-
   return (
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
+      <div>
+        {answers.map((answer, index) => {
+          const isCorrect = index === correctIndex;
+          return (
+            <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+              {answer}
+            </button>
+          );
+        })}
+      </div>
       <h5>{timeRemaining} seconds remaining</h5>
     </>
   );
